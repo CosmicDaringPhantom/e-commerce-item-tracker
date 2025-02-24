737eb87c7eff64d4d47744f0b3f753c01b6c2425
= <=>
2025-02-24 13:28:58 +0800

    Implemented past message pre prompting

--- OpenAi/ChatController.py
+++ OpenAi/ChatController.py
@@ -1,13 +1,31 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import json
 
 load_dotenv()
 
 key = os.getenv("API_KEY")
 client = OpenAI(api_key = key)
 
+def load_json():
+    with open('history.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+def write_json(data, file):
+    with open(f'{file}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def get_messages():
+    prev_messages = load_json()
+
+    return prev_messages["Messages"]
+
 def send_prompt(message):
+
+    prev_messages = load_json()
+
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -19,16 +37,26 @@ def send_prompt(message):
         ]
     )
 
-    print("loading answer.....")
-    return completion.choices[0].message.content
+    message_val = {"role": "user", "content": message}
+    prev_messages["Messages"].append(message_val)
+
+    resp = completion.choices[0].message.content
+    resp_val = {"role": "system", "content": resp}
+    prev_messages["Messages"].append(resp_val)
+
+    write_json(prev_messages, "history")
+    return resp
+
+def new_chat():
+    write_json({"Messages": []}, "history")
+
 
 def pre_prompting(prompt):
+
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": prompt},
+        messages=[{"role": "system", "content": prompt}
         ]
     )
 
     return completion.choices[0].message.content
-
